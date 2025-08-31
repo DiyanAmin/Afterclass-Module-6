0525a8f88e7c4465978b967bd993e0463f2f1576
DiyanAmin <pakuptl3112@gmail.com>
2025-08-31 14:02:34 +0530

    done

--- LetsAddSprites.py
+++ LetsAddSprites.py
@@ -6,17 +6,46 @@ pygame.init()
 # Screen setup
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Sprites ')
+pygame.display.set_caption('Sprites')
 
 # Colors
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 
-# Sprites
-player = pygame.Rect(100, 100, 50, 50)  
-enemy = pygame.Rect(300, 300, 50, 50)   
-speed = 6 # --> To increase movement speed use this!
+# Speed
+speed = 6
+
+# Sprite Classes
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+    def update(self, keys):
+        if keys[pygame.K_w]:
+            self.rect.y -= speed
+        if keys[pygame.K_s]:
+            self.rect.y += speed
+        if keys[pygame.K_a]:
+            self.rect.x -= speed
+        if keys[pygame.K_d]:
+            self.rect.x += speed
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+# Sprite Groups
+all_sprites = pygame.sprite.Group()
+player = Player(100, 100)
+enemy = Enemy(300, 300)
+all_sprites.add(player, enemy)
 
 # Clock
 clock = pygame.time.Clock()
@@ -30,21 +59,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Movement keys
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player.y -= speed
-    if keys[pygame.K_s]:
-        player.y += speed
-    if keys[pygame.K_a]:
-        player.x -= speed
-    if keys[pygame.K_d]:
-        player.x += speed
+    player.update(keys)
 
-    # Draw sprites
-    pygame.draw.rect(screen, BLUE, player)
-    pygame.draw.rect(screen, RED, enemy)
-
+    all_sprites.draw(screen)
     pygame.display.update()
     clock.tick(60)
 
